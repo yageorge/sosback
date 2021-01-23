9e7b2054d7cd193b6574b43472318bb6b2199b90
YaGeorge <gyaacoub@gmail.com>
2021-01-23 18:37:25 +0200

    update CHART live data + clean code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Firebase\Auth\Token\Exception\InvalidToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Company;
--- app/Http/Controllers/CloudMessagingController.php
+++ app/Http/Controllers/CloudMessagingController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Exception;
 
-use App\Models\User;
 use App\Models\Department;
 
 class CloudMessagingController extends Controller
